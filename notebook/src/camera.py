378442c4a05022d82af97e4682261881429af71c
RamadanAhmed <ramadan.ahmedr93@gmail.com>
2021-03-30 14:10:29 +0200

    Add get_projected_image

--- notebook/src/camera.py
+++ notebook/src/camera.py
@@ -36,6 +36,7 @@ class Camera:
                 self.visualizer.show_dotted_image(warped, self.car_points)
             return warped
         return None
+    
     def get_bird_view(self, car_view_image, show_dotted = False):
         if self.do_prespective:
             img_size = (car_view_image.shape[1], car_view_image.shape[0])
@@ -46,6 +47,7 @@ class Camera:
                 self.visualizer.show_dotted_image(warped, self.bird_points)
             return warped
         return None
+    
     def get_undistort_image(self, dist_image, show_dotted = False):
         undist = cv2.undistort(dist_image, self.camera_matrix,
                                self.distortion_coefficient, None, self.camera_matrix)
@@ -53,3 +55,21 @@ class Camera:
         if show_dotted: 
             self.visualizer.show_dotted_image(undist, self.car_points)
         return undist
+
+    def get_projected_image(self, undist, warped, ploty, left_fitx, right_fitx):
+        warp_zero = np.zeros_like(warped).astype(np.uint8)
+        color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+        # Recast the x and y points into usable format for cv2.fillPoly()
+        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+        pts = np.hstack((pts_left, pts_right))
+
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+        newwarp = self.get_car_view(color_warp)
+        # Combine the result with the original image
+
+        result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+        return result
